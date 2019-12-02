cf1dae6d62b20079b60692d9f45a9a3b164e680d
mengxinxin <andy_m129@163.com>
2019-12-02 15:07:05 +0800

    添加安装脚本，及说明文档

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CodeSnippets
-常用的代码模板/片断
+* 常用的代码模板/片断
+* 可以下载 `install_codesnippets.sh`  文件，
+  直接通过 `sh install_codesnippets.sh` 命令，在任意位置 安装该库到 Xcode 的目录下， 重启后即可生效使用
+
---
+++ install_codesnippets.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+debug=NO
+
+# Xcode 下代码片段的路径
+xcode_codesnippets_path="$HOME/Library/Developer/Xcode/UserData/CodeSnippets"
+if [ $debug ];then
+    xcode_codesnippets_path="$HOME/Desktop/CodeSnippets"
+fi
+echo "\n准备为您安装 CodeSnippets，路径为：${xcode_codesnippets_path}"
+
+# 若目录已存在，则先备份
+if [ -d $xcode_codesnippets_path ];then
+
+    # 当前时间
+    current_datetime="`date +%Y%m%d%H%M%s`"
+    if [ $debug ];then
+        echo "当前时间：$current_datetime"
+    fi
+
+    # 备份路径
+    xcode_codesnippets_bakpath="${xcode_codesnippets_path}.bak${current_datetime}"
+	mv "$xcode_codesnippets_path" "$xcode_codesnippets_bakpath"
+	echo "CodeSnippets 文件夹存在，已为您进行备份：${xcode_codesnippets_bakpath}"
+fi
+
+# 拉取最新代码片段
+echo "\n开始拉取最新的 CodeSnippets ..."
+git clone https://github.com/AndyM129/CodeSnippets.git $xcode_codesnippets_path
+echo "已成功拉取最新的 CodeSnippets，须重启 Xcode 后生效。\n\n"
